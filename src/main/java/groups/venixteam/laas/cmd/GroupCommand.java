3c8d1c50e6efa23b3ca243527761d3499eb00a2f
Laas <doutorlaas@gmail.com>
2025-04-18 18:18:40 -0300

    venixgroup 2.0

--- src/main/java/groups/venixteam/laas/cmd/GroupCommand.java
+++ src/main/java/groups/venixteam/laas/cmd/GroupCommand.java
@@ -40,7 +40,7 @@ public class GroupCommand implements CommandExecutor {
         LuckPerms luckPerms = LuckPermsProvider.get();
 
         if (!player.hasPermission("venixgroup.group")) {
-            player.sendMessage(Language.permission$error);
+            player.sendMessage(Language.permissionError);
             return true;
         }
 
@@ -55,28 +55,28 @@ public class GroupCommand implements CommandExecutor {
         Set<Group> groups = luckPerms.getGroupManager().getLoadedGroups();
 
         if (group == null || groups.stream().noneMatch(g -> g.getName().equalsIgnoreCase(group))) {
-            String availableGroups = groups.stream().map(Group::getName).collect(Collectors.joining(", "));
-            player.sendMessage("§6§lGROUP: §cGrupos disponíveis: §e\n" + availableGroups);
+            String listGroups = groups.stream().map(Group::getName).collect(Collectors.joining(", "));
+            player.sendMessage("§6§lGROUP: §cGrupos disponíveis: §e\n" + listGroups);
             player.playSound(player.getLocation(), Sound.NOTE_BASS, 0.5f,2.0f);
             return true;
         }
 
         if (!sender.isOp() && targetName.equals(sender.getName())) {
-            player.sendMessage(Language.playergroup$error);
+            player.sendMessage(Language.playerGroupError);
             player.playSound(player.getLocation(), Sound.NOTE_BASS, 0.5f,2.0f);
             return true;
         }
 
         Player target = Bukkit.getServer().getPlayerExact(targetName);
         if (target == null) {
-            player.sendMessage(Language.player$error);
+            player.sendMessage(Language.playerError);
             player.playSound(player.getLocation(), Sound.NOTE_BASS, 0.5f,2.0f);
             return true;
         }
 
         User user = luckPerms.getUserManager().getUser(target.getUniqueId());
         if (user == null) {
-            player.sendMessage(Language.playerinfo$error);
+            player.sendMessage(Language.playerInfoError);
             player.playSound(player.getLocation(), Sound.NOTE_BASS, 0.5f,2.0f);
             return true;
         }
@@ -84,48 +84,46 @@ public class GroupCommand implements CommandExecutor {
         switch (action.toLowerCase()) {
             case "adicionar":
                 if (hasGroup(user, group)) {
-                    player.sendMessage(Language.playergroup$has.replace("{GROUP}", group.toUpperCase()));
+                    player.sendMessage(Language.playerGroupHas.replace("{GROUP}", group.toUpperCase()));
                     player.playSound(player.getLocation(), Sound.NOTE_BASS, 0.5f, 2.0f);
                     return true;
                 }
 
-                if (VIPS.contains(group) && Language.enable$viptitle$active == true) {
+                if (VIPS.contains(group) && Language.enableVipTitleActive == true) {
                     TitleAPI titleAPI = new TitleAPI();
-                    titleAPI.send(player, Language.$viptitle$fadein, Language.$viptitle$showtime, Language.$viptitle$fadeout,
-                            Language.viptitle$header.replace("{PLAYER}", target.getName()).replace("{GROUP}", group.toUpperCase()),
-                            Language.viptitle$footer.replace("{PLAYER}", target.getName()).replace("{GROUP}", group.toUpperCase()));
+                    titleAPI.send(player, Language.vipTitleFadein, Language.vipTitleShowtime, Language.vipTitleFadeout,
+                            Language.vipTitleHeader.replace("{PLAYER}", target.getName()).replace("{GROUP}", group.toUpperCase()),
+                            Language.vipTitleFooter.replace("{PLAYER}", target.getName()).replace("{GROUP}", group.toUpperCase()));
 
-                    vipSetted(target.getLocation(), 1, 5);
+                    vipSet(target.getLocation(), 1, 5);
                     for (Player players : Bukkit.getOnlinePlayers()) {
                         players.playSound(players.getLocation(), Sound.ENDERDRAGON_GROWL, 0.5f, 5.0f);
                     }
 
                     user.data().add(InheritanceNode.builder(group).build());
                     user.setPrimaryGroup(group);
-                    player.sendMessage(Language.playergroup$set.replace("{PLAYER}", target.getName()).replace("{GROUP}", group.toUpperCase()));
+                    player.sendMessage(Language.playerGroupSet.replace("{PLAYER}", target.getName()).replace("{GROUP}", group.toUpperCase()));
                     player.playSound(player.getLocation(), Sound.SUCCESSFUL_HIT, 0.5f,2.0f);
                 } else {
                     user.data().add(InheritanceNode.builder(group).build());
-                    player.sendMessage(Language.playergroup$set.replace("{PLAYER}", target.getName()).replace("{GROUP}", group.toUpperCase()));
+                    player.sendMessage(Language.playerGroupSet.replace("{PLAYER}", target.getName()).replace("{GROUP}", group.toUpperCase()));
                     player.playSound(player.getLocation(), Sound.SUCCESSFUL_HIT, 0.5f,2.0f);
                 }
                 break;
             case "remover":
                 user.data().remove(InheritanceNode.builder(group).build());
-                player.sendMessage(Language.playergroup$remove.replace("{PLAYER}", target.getName()).replace("{GROUP}", group.toUpperCase()));
+                player.sendMessage(Language.playerGroupRemove.replace("{PLAYER}", target.getName()).replace("{GROUP}", group.toUpperCase()));
                 player.playSound(player.getLocation(), Sound.SUCCESSFUL_HIT, 0.5f,2.0f);
                 break;
             default:
-                sender.sendMessage("§cUtilize 'adicionar' ou 'remover'.");
+                player.sendMessage("§c§lERRO: §cUtilize: §f/group <adicionar/remover> <player> <group>");
                 return true;
         }
-
         luckPerms.getUserManager().saveUser(user);
-
         return true;
     }
 
-    public void vipSetted(final Location location, final int count, int power) {
+    public void vipSet(final Location location, final int count, int power) {
         Firework firework = (Firework) location.getWorld().spawnEntity(location, EntityType.FIREWORK);
         FireworkMeta meta = firework.getFireworkMeta();
         FireworkEffect effect = FireworkEffect.builder()
